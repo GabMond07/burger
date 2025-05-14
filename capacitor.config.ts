1c6c536637e1a056d3ad5ebbb5399a2c651a95b9
EricGgarcia <146622118+EricGgarcia@users.noreply.github.com>
2025-05-13 19:31:55 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pf2_burguer_queen',
+  webDir: 'www'
+};
+
+export default config;

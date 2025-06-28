e04847b65ecb4336ec7afec5b5f0203ceea4ef60
Josue Santamaria <josuexsanta@gmail.com>
2025-06-28 17:08:46 -0600

    oopsx2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   urlApi: 'http://192.168.100.8:3000/api/v1/',
   publihKey:
-  'pk_test_51RPR6wCL0PlDamqNW7HiNJ4Lbl6B0kThMMXIOt8bv4wctLT2ovptzZrHVf1u7WPbM0GvtUoCLjYpEBZaNY69TJ3v00RWJSMIfW',
+  '',
   secretKey:
-  'sk_test_51RPR6wCL0PlDamqNAXJP1qLxlTrf2K254wMbEkmoWtnps5zdzQrWBxg1jh7jdlgEHESiC2qhR6O1cjo1ZAj33Otu00VjkMEZ7n'
+  ''
 };
